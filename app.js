1b9f6e391a65412fa6293d9398de66cb89b8c3d6
ruralkan <ruralkan@gmail.com>
2019-07-25 13:17:20 -0500

    Server On and tests passed

---
+++ app.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
+const indexRouter = require('./routes/index')
+const spidyRouter = require('./routes/spidy');
+
+
+
+// Create Express application
+module.exports = ({ logger }) => express()
+// Install Middleware
+.use(bodyParser.urlencoded({ extended: true }))
+.use(bodyParser.json())
+.use((req, res, next) => {
+  req.base = `${req.protocol}://${req.get('host')}`
+  req.logger = logger
+ // req.db = database
+  return next()
+})
+.use(morgan('tiny'))
+
+
+
+// Path routing
+.use(indexRouter)
+.use('/test', spidyRouter)
+
+
+// Error handling
+.use((error, req, res, next) => {
+  logger.error(error, error)
+  res.status(error.status || 500).json({ error });
+  next();
+});
--- test/server.test.js
+++ test/server.test.js
@@ -1,4 +1,10 @@
 import {expect} from 'chai';
+import server from '../bin/server'
+
+
+const addr = server.address()
+const bind = typeof addr === 'string' ? `pipe ${addr}` : `port ${addr.port}`
+console.log(bind);
 
 
 describe('Our first test', () => {
@@ -8,8 +14,16 @@ describe('Our first test', () => {
 });
 
 describe('server working', () => {
-    it('should exist');
-    it('should be an object');
-    it('tests that server is running current port');
+    it('should exist', ()=> {
+        expect(server).to.not.be.an('undefined');
+        //expect(server).toBeDefined();
+      });
+    it('should be an object', ()=>{
+        expect(server).to.be.an('object');
+        //expect(typeof server).toBe('object');
+      });
+    it('tests that server is running current port', ()=>{
+        expect(bind).to.equal('port 3000')
+    });
 });
 
