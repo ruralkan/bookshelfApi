327f7c5e24a7c2fab2c7ba3e7378f88644e47d37
ruralkan <ruralkan@gmail.com>
2019-07-25 21:42:02 -0500

    fix errors

---
+++ features/step_definitions/deleteBook.js
@@ -0,0 +1,16 @@
+const { Given, When, Then } = require('cucumber');
+
+Given('an user watn to delete a book from the collection', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+When('request Delete', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Then('I can Delete the book', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
